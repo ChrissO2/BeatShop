b2d239fef9d5cb6a9fb3272d8a7b74389ce59cf4
Krzysztof Otręba <krzysztof.otreba02@gmail.com>
2023-06-19 02:22:48 +0200

    added checkout and orders views, added needed urls

--- beatshop/beatshop/urls.py
+++ beatshop/beatshop/urls.py
@@ -21,5 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
     path('api/', include('shop.urls')),
+    path('api/', include('order.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
