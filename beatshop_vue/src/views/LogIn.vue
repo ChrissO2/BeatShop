3fd63e8d0cc0c85fde2c1055ac1fe93234856a5c
Krzysztof Otręba <krzysztof.otreba02@gmail.com>
2023-06-22 18:24:29 +0200

    change title in login

--- beatshop_vue/src/views/LogIn.vue
+++ beatshop_vue/src/views/LogIn.vue
@@ -51,7 +51,7 @@ export default {
         }
     },
     mounted() {
-        document.title = 'Log In | Djackets'
+        document.title = 'Log In | BeatStore'
     },
     methods: {
         async submitForm() {
